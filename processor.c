0e08a4826d1c671d87aeea7bf5c5e5f598589cc2
Andrew Yu <andrewey@uci.edu>
2020-10-14 16:21:26 -0700

    implemeneted generate_request(still needs testing) and calc_time_cumulative_avg; added new fields to processor struct

--- processor.c
+++ processor.c
@@ -11,13 +11,39 @@ int getAccessTime(Processor * p)
 }
 
 /**
- * args: -mSize : number of memory modules
+ * args: -proc  : processor that generates the request
+ *       -mSize : number of memory modules
  *       -d     : the type of distribution to sample from
  *                'u' for uniform distribution sampling
  *                'n' for normal distribution sampling
  */
-int generate_request(int mSize, char d)
+int generate_request(Processor * proc, int mSize, char d)
 {
-  printf("TODO: generate_request not implemented yet\n");
-  return 0;
+  // return variable
+  int new_mem_module_req_index = 0;
+
+  // decide type of distribution to sample from
+  if(d == 'n') // normal sampling
+  {
+    // generate a normally distributed number within norm(mu=proc->mmr, sd=mSize)
+    new_mem_module_req_index = sample_norm()
+                               * mSize                      // set std-dev here
+                               + (proc->mem_mod_requested); // set mean here
+  }
+  else // uniform sampling
+  {
+    // generate a uniformly distributed number within [0, mSize]
+    new_mem_module_req_index = sample_unif() * mSize;
+  }
+
+  // return new index within number of memory modules
+  return new_mem_module_req_index % mSize;
+}
+
+double calc_time_cumulative_avg(Processor * proc, int curr_cycle)
+{
+  double proc_AT = (double) getAccessTime(proc);
+  if(proc_AT == 0)
+    return 0;
+  return curr_cycle / proc_AT;
 }
--- processor.h
+++ processor.h
@@ -6,10 +6,10 @@
 
 struct processor
 {
-  // int id;
   int access_time;
-  // MemoryMod * prevRequest;
-}
+  int mem_mod_requested;
+  double time_cumulative_avg;
+};
 typedef struct processor Processor;
 
 void incrAccessTime(Processor * p);
@@ -17,11 +17,14 @@ void incrAccessTime(Processor * p);
 int getAccessTime(Processor * p);
 
 /**
- * args: -mSize : number of memory modules
+ * args: -proc  : processor that generates the request
+ *       -mSize : number of memory modules
  *       -d     : the type of distribution to sample from
  *                'u' for uniform distribution sampling
  *                'n' for normal distribution sampling
  */
-int generate_request(int mSize, char d);
+int generate_request(Processor * proc, int mSize, char d);
+
+double calc_time_cumulative_avg(Processor * proc, int curr_cycle);
 
 #endif
